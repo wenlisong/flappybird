fb4afff68060ebd288cc2a405192605bba46d1c7
songwenli <327361201@qq.com>
2018-11-03 09:49:27 +0800

    1.observe step 2.inital epsilon

--- deep_q_network.py
+++ deep_q_network.py
@@ -9,10 +9,10 @@ from collections import deque
 GAME = 'bird'  # the name of the game being played for log files
 ACTIONS = 2  # number of valid actions
 GAMMA = 0.99  # decay rate of past observations
-OBSERVE = 100.  # timesteps to observe before training
+OBSERVE = 10000.  # timesteps to observe before training
 EXPLORE = 3000000.  # frames over which to anneal epsilon
 FINAL_EPSILON = 0.0001  # final value of epsilon
-INITIAL_EPSILON = 0.9  # starting value of epsilon
+INITIAL_EPSILON = 0.1  # starting value of epsilon
 REPLAY_MEMORY = 50000  # number of previous transitions to remember
 BATCH = 32  # size of minibatch
 FRAME_PER_ACTION = 1
